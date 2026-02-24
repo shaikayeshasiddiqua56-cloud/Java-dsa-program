050dcc66b7d10247c7c9607e82ebb65e1e5eca55
shaikayeshasiddiqua56-cloud <shaikayeshasiddiqua56@gmail.com>
2026-02-24 09:45:07 +0530

    Create PostfixEvaluation.java

---
+++ Day-24 Stack And Queue/PostfixEvaluation.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+public class PostfixEvaluation {
+    static int evaluate(String exp) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (Character.isDigit(ch)) {
+                st.push(ch - '0');
+            }
+            else {
+                int val2 = st.pop();
+                int val1 = st.pop();
+                switch (ch) {
+                    case '+':
+                        st.push(val1 + val2);
+                        break;
+                    case '-':
+                        st.push(val1 - val2);
+                        break;
+                    case '*':
+                        st.push(val1 * val2);
+                        break;
+                    case '/':
+                        st.push(val1 / val2);
+                        break;
+                }
+            }
+        }
+        return st.pop();
+    }
+    public static void main(String[] args) {
+        String exp = "23*54*+9-";
+        System.out.println("Result: " + evaluate(exp));
+    }
+}
